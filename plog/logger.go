365974a423961c7e7a0cabac9cffe641587e8a7e
leeprince <leeprince@foxmail.com>
2022-03-30 00:50:02 +0800

    feature: 实现 github.com/uber/jaeger-client-go@logger.go 的 Logger 接口

--- plog/logger.go
+++ plog/logger.go
@@ -92,10 +92,13 @@ func WithContext(ctx context.Context) *logrus.Entry {
 
 
 // --- 实现 logger 接口 ---
+// 实现 github.com/uber/jaeger-client-go@logger.go 的 Logger 接口
 func (p Plog) Error(msg string) {
 	Error(msg)
 }
 func (p Plog) Infof(msg string, args ...interface{}) {
 	Infof(msg, args...)
 }
+// 实现 github.com/uber/jaeger-client-go@logger.go 的 Logger 接口 -end
+
 // --- 实现 logger 接口 -end ---
