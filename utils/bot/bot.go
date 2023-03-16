56c4d93e9213c70a524d6868ee91ef24db6b2468
prince.lee <prince.lee@fowo.com>
2023-03-16 20:28:39 +0800

    feature: bot

---
+++ utils/bot/bot.go
@@ -0,0 +1,123 @@
+package bot
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/leeprince/goinfra/http/httpcli"
+	"github.com/leeprince/goinfra/utils"
+	"github.com/pkg/errors"
+	"net/http"
+)
+
+/**
+ * @Author: prince.lee <leeprince@foxmail.com>
+ * @Date:   2023/3/16 18:13
+ * @Desc:
+ */
+
+type BotContentType string
+
+const (
+	BOT_CONTENTTYPE_TEXT     BotContentType = "text"
+	BOT_CONTENTTYPE_MARKDOWN BotContentType = "markdown"
+)
+
+type TextBody struct {
+	Msgtype string `json:"msgtype"`
+	Text    struct {
+		Content string `json:"content"`
+	} `json:"text"`
+}
+
+type MarkdownBody struct {
+	Msgtype  string `json:"msgtype"`
+	Markdown struct {
+		Content string `json:"content"`
+	} `json:"markdown"`
+}
+
+// contextType
+// 	contextType=BOT_CONTENTTYPE_TEXT,则content的内容自动以中文逗号分割（不支持换行）
+// 	contextType=BOT_CONTENTTYPE_MARKDOWN,则content的内容以换行符分割（支持换行）
+func SendQYWXBot(url string, contextType BotContentType, title string, contents []string) (respBobyBytes []byte, resp *http.Response, err error) {
+	if url == "" {
+		err = errors.New("SendQYWXBot url 必填")
+		return
+	}
+	if title == "" {
+		err = errors.New("SendQYWXBot title 必填")
+		return
+	}
+
+	if !utils.InString(string(contextType), []string{
+		string(BOT_CONTENTTYPE_TEXT),
+		string(BOT_CONTENTTYPE_MARKDOWN),
+	}) {
+		err = errors.New("SendQYWXBot contextType 暂不支持")
+		return
+	}
+
+	// 组装默认格式
+	var sendContentBuf *bytes.Buffer
+	if contextType == BOT_CONTENTTYPE_TEXT {
+		sendContentBuf = bytes.NewBufferString(title)
+		sendContentBuf.WriteString("。")
+	} else {
+		sendContentBuf = bytes.NewBufferString("<font color=\"warning\">")
+		sendContentBuf.WriteString(title)
+		sendContentBuf.WriteString("</font>\n")
+	}
+	for i, content := range contents {
+		if contextType == BOT_CONTENTTYPE_TEXT {
+			sendContentBuf.WriteString(content)
+			if i != len(contents)-1 {
+				sendContentBuf.WriteString("，")
+			}
+			continue
+		}
+		sendContentBuf.WriteString("- ")
+		sendContentBuf.WriteString(content)
+		if i != len(contents)-1 {
+			sendContentBuf.WriteString("\n")
+		}
+	}
+	sendContent := sendContentBuf.String()
+
+	var sendBodyByte []byte
+	if contextType == BOT_CONTENTTYPE_TEXT {
+		sendBody := TextBody{
+			Msgtype: string(BOT_CONTENTTYPE_TEXT),
+			Text: struct {
+				Content string `json:"content"`
+			}{
+				Content: sendContent,
+			},
+		}
+
+		sendBodyByte, err = json.Marshal(sendBody)
+		if err != nil {
+			return
+		}
+	} else {
+		sendBody := MarkdownBody{
+			Msgtype: string(BOT_CONTENTTYPE_MARKDOWN),
+			Markdown: struct {
+				Content string `json:"content"`
+			}{
+				Content: sendContent,
+			},
+		}
+
+		sendBodyByte, err = json.Marshal(sendBody)
+		if err != nil {
+			return
+		}
+	}
+
+	//发送数据
+	return httpcli.NewHttpClient().
+		WithURL(url).
+		WithMethod(http.MethodPost).
+		WithBody(sendBodyByte).
+		Do()
+}
---
+++ utils/bot/bot_test.go
@@ -0,0 +1,75 @@
+package bot
+
+import (
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+/**
+ * @Author: prince.lee <leeprince@foxmail.com>
+ * @Date:   2023/3/16 20:16
+ * @Desc:
+ */
+
+const botUrl = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=xxxxxxxx"
+
+func TestSendQYWXBot(t *testing.T) {
+	type args struct {
+		url         string
+		contextType BotContentType
+		title       string
+		contents    []string
+	}
+	tests := []struct {
+		name              string
+		args              args
+		wantRespBobyBytes []byte
+		wantResp          *http.Response
+		wantErr           bool
+	}{
+		{
+			name: "",
+			args: args{
+				url:         botUrl,
+				contextType: BOT_CONTENTTYPE_TEXT,
+				title:       "登录次数超过限制",
+				contents: []string{
+					"税号：91441302MA53J2H49U",
+				},
+			},
+			wantRespBobyBytes: nil,
+			wantResp:          nil,
+			wantErr:           false,
+		},
+		{
+			name: "",
+			args: args{
+				url:         botUrl,
+				contextType: BOT_CONTENTTYPE_MARKDOWN,
+				title:       "登录次数超过限制",
+				contents: []string{
+					"税号：91441302MA53J2H49U",
+				},
+			},
+			wantRespBobyBytes: nil,
+			wantResp:          nil,
+			wantErr:           false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotRespBobyBytes, gotResp, err := SendQYWXBot(tt.args.url, tt.args.contextType, tt.args.title, tt.args.contents)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("SendQYWXBot() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotRespBobyBytes, tt.wantRespBobyBytes) {
+				t.Errorf("SendQYWXBot() gotRespBobyBytes = %v, want %v", gotRespBobyBytes, tt.wantRespBobyBytes)
+			}
+			if !reflect.DeepEqual(gotResp, tt.wantResp) {
+				t.Errorf("SendQYWXBot() gotResp = %v, want %v", gotResp, tt.wantResp)
+			}
+		})
+	}
+}
