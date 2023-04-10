aa4c9ba39524da116eadcdd5d344209da3a0188f
leeprince <leeprince@foxmail.com>
2023-04-10 20:55:20 +0800

    feature: 超时重传

--- task/retry/timeout_retry.go
+++ task/retry/timeout_retry.go
@@ -11,7 +11,7 @@ import "time"
  *				2. 如果长时间未收到响应则说明，taskFunc方法已经处理超时，需要重新执行taskFunc方法
  */
 
-// 使用golang的select语句和time.After()函数来实现超时重传机制。具体来说，您可以创建一个channel，然后使用select语句监听该channel和time.After()函数返回的channel，如果在指定时间内没有收到响应，则会从time.After()函数返回的channel中读取数据，然后重新执行send方法。以下是一个示例代码：¹
+// 使用golang的select语句和time.After()函数来实现超时重传机制。具体来说，您可以创建一个channel，然后使用select语句监听该channel和time.After()函数返回的channel，如果在指定时间内没有收到响应，则会从time.After()函数返回的channel中读取数据，然后重新执行taskFunc方法。以下是一个示例代码：¹
 
 func SendWithTimeout(timeout time.Duration, taskFunc func() error) error {
 	ch := make(chan error)
