4663eb7afe54e4ab1f20eb472dc027ce921928fa
prince.lee <prince.lee@fowo.com>
2023-07-28 10:49:09 +0800

    feature: return_error_test.go

--- test/return_error_test.go
+++ test/return_error_test.go
@@ -29,3 +29,65 @@ func ReturnErr01() (err error) {
 func ReturnErr02() (s string, err error) {
 	return "", errors.New("ReturnErr02")
 }
+
+func TestReturnOtherScopeErr(t *testing.T) {
+	err := ReturnOtherScopeErr01()
+	fmt.Println("ReturnOtherScopeErr01:", err)
+
+	// 会报错：因为在其他作用域的使用了返回参数的变量，进行了重新声明赋值，导致报错。
+	// 解决：在其他作用域的使用了返回参数的变量时，不要重新声明，只需赋值。
+	//	如：var s string; s, err = xxx();
+	//	如：s, serr := xxx(); err = serr;
+	err = ReturnOtherScopeErr0101()
+	fmt.Println("ReturnOtherScopeErr0101:", err)
+}
+
+func ReturnOtherScopeErr01() (err error) {
+	s, err := ReturnErr02()
+	if err != nil {
+		fmt.Println("ReturnErr02 err:", err)
+		return
+	}
+	fmt.Println(s)
+	return
+}
+func ReturnOtherScopeErr0101() (err error) {
+	/*for i := 0; i < 3; i++ {
+		// 报错：inner declaration of var err error
+		s, err := ReturnErr02()
+		if err != nil {
+			// 报错：result parameter err not in scope at return
+			fmt.Println("ReturnErr02 err:", err)
+			return
+		}
+		// 报错：result parameter err not in scope at return
+		fmt.Println(s)
+		return
+	}*/
+
+	// 解决：方式一；var s string; s, err = xxx();
+	for i := 0; i < 3; i++ {
+		var s string
+		s, err = ReturnErr02()
+		if err != nil {
+			fmt.Println("ReturnErr02 err:", err)
+			return
+		}
+		fmt.Println(s)
+		return
+	}
+
+	// 解决：方式二；s, serr := xxx(); err = serr;
+	for i := 0; i < 3; i++ {
+		s, serr := ReturnErr02()
+		if serr != nil {
+			err = serr
+			fmt.Println("ReturnErr02 err:", err)
+			return
+		}
+		fmt.Println(s)
+		return
+	}
+
+	return
+}
