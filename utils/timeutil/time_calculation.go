b4a553e704c3c7831b7c6ba2a97c598977678c13
prince.lee <prince.lee@fowo.com>
2023-07-28 18:51:19 +0800

    feature: 时间计算

--- utils/timeutil/time_calculation.go
+++ utils/timeutil/time_calculation.go
@@ -1,18 +1,26 @@
 package timeutil
 
-import (
-	"time"
-)
+import "time"
 
 /**
- * @Author: prince.lee
- * @Date:   2022/3/24 17:12
- * @Desc:   时间
+ * @Author: prince.lee <leeprince@foxmail.com>
+ * @Date:   2023/7/28 18:47
+ * @Desc:
  */
 
-// 是否使用毫秒为单位
-//
-//	小于秒的单位时间则使用毫秒
-func UseMillisecondUnit(dur time.Duration) bool {
-	return dur < time.Second || dur%time.Second != 0
+// 计算过去的时间
+func GetBeforeData(day int) time.Time {
+	// 获取当前时间
+	currentTime := time.Now()
+
+	beforDay := day * -1
+	return currentTime.AddDate(0, 0, beforDay)
+}
+
+// 计算当前月1日的时间
+func GetMonthFirthDay() time.Time {
+	// 获取当前时间
+	currentTime := time.Now()
+
+	return time.Date(currentTime.Year(), currentTime.Month(), 1, 0, 0, 0, 0, currentTime.Location())
 }
