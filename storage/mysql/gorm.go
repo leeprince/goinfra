97bd701f0912bc5f19e1b7ea3be612417a242045
prince.lee <prince.lee@fowo.com>
2023-08-02 14:51:37 +0800

    feature: init mysql client

--- storage/mysql/gorm.go
+++ storage/mysql/gorm.go
@@ -1,11 +1,11 @@
 package pgorm
 
 import (
-    "github.com/pkg/errors"
-    "gorm.io/driver/mysql"
-    "gorm.io/gorm"
-    "gorm.io/gorm/logger"
-    "time"
+	"github.com/pkg/errors"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"time"
 )
 
 /**
@@ -18,85 +18,97 @@ import (
 var mysqlClients map[string]MysqlClient
 
 type MysqlClient struct {
-    db  *gorm.DB
+	db *gorm.DB
 }
 
 type MysqlConfs map[string]MysqlConf
 type MysqlConf struct {
-    Dsn             string           `yaml:"dsn" json:"dsn"`         // eg: "user:password@/dbname?charset=utf8&parseTime=True&loc=Local"
-    IsDebug         bool             `yaml:"isDebug" json:"isDebug"` // 调试启动调试模式
-    MaxOpenConns    int              `yaml:"maxOpenConn" json:"maxOpenConn"`
-    MaxIdleConns    int              `yaml:"maxIdleConn" json:"maxIdleConn"`
-    ConnMaxLifetime time.Duration    `yaml:"connMaxLifetime" json:"connMaxLifetime"`
-    Logger          logger.Interface `yaml:"-" json:"-"`
+	Dsn             string           `yaml:"dsn" json:"dsn"`         // eg: "user:password@/dbname?charset=utf8&parseTime=True&loc=Local"
+	IsDebug         bool             `yaml:"isDebug" json:"isDebug"` // 调试启动调试模式
+	MaxOpenConns    int              `yaml:"maxOpenConn" json:"maxOpenConn"`
+	MaxIdleConns    int              `yaml:"maxIdleConn" json:"maxIdleConn"`
+	ConnMaxLifetime time.Duration    `yaml:"connMaxLifetime" json:"connMaxLifetime"`
+	Logger          logger.Interface `yaml:"-" json:"-"`
 }
 
-func InitMysqlClient(confs MysqlConfs) (err error) {
-    mysqlClients = make(map[string]MysqlClient, len(confs))
-    
-    for name, conf := range confs {
-        if err = checkMysqlConf(conf); err != nil {
-            return errors.Wrap(err, "checkMysqlConf error")
-        }
-        
-        db, err := gorm.Open(mysql.Open(conf.Dsn), &gorm.Config{
-            PrepareStmt: false,
-            Logger:      conf.Logger,
-        })
-        if err != nil {
-            return errors.Wrap(err, "gorm.Open error")
-        }
-        
-        if conf.IsDebug {
-            db.Debug()
-        }
-        
-        if err = setSqlConf(db, conf); err != nil {
-            return errors.Wrap(err, "setSqlConf error")
-        }
-        
-        mysqlClients[name] = MysqlClient{
-            db:  db,
-        }
-    }
-    
-    return
+func InitMysqlClientMap(confs MysqlConfs) (err error) {
+	mysqlClients = make(map[string]MysqlClient, len(confs))
+
+	for name, conf := range confs {
+		db, initErr := InitMysqlClient(conf)
+		if initErr != nil {
+			err = errors.Wrap(initErr, "InitMysqlClient error")
+			return
+		}
+
+		mysqlClients[name] = MysqlClient{
+			db: db,
+		}
+	}
+
+	return
+}
+
+func InitMysqlClient(conf MysqlConf) (db *gorm.DB, err error) {
+	if err = checkMysqlConf(conf); err != nil {
+		err = errors.Wrap(err, "checkMysqlConf error")
+		return
+	}
+
+	db, err = gorm.Open(mysql.Open(conf.Dsn), &gorm.Config{
+		PrepareStmt: false,
+		Logger:      conf.Logger,
+	})
+	if err != nil {
+		err = errors.Wrap(err, "gorm.Open error")
+		return
+	}
+
+	if conf.IsDebug {
+		db.Debug()
+	}
+
+	if err = setSqlConf(db, conf); err != nil {
+		err = errors.Wrap(err, "setSqlConf error")
+	}
+
+	return
 }
 
 func checkMysqlConf(conf MysqlConf) error {
-    if conf.Dsn == "" {
-        return errors.New("dsn must not empty")
-    }
-    return nil
+	if conf.Dsn == "" {
+		return errors.New("dsn must not empty")
+	}
+	return nil
 }
 
 func setSqlConf(db *gorm.DB, conf MysqlConf) error {
-    sqlDB, err := db.DB()
-    if err != nil {
-        return err
-    }
-    // SetMaxIdleConns 设置空闲连接池中连接的最大数量
-    sqlDB.SetMaxIdleConns(MaxIdleConns)
-    if conf.MaxIdleConns > 0 {
-        sqlDB.SetMaxIdleConns(conf.MaxIdleConns)
-    }
-    // SetMaxOpenConns 设置打开数据库连接的最大数量。
-    sqlDB.SetMaxOpenConns(MaxOpenConns)
-    if conf.MaxOpenConns > 0 {
-        sqlDB.SetMaxOpenConns(conf.MaxOpenConns)
-    }
-    // SetConnMaxLifetime 设置了连接可复用的最大时间。
-    sqlDB.SetConnMaxLifetime(ConnMaxLifetime)
-    if conf.ConnMaxLifetime > 0 {
-        sqlDB.SetConnMaxLifetime(conf.ConnMaxLifetime)
-    }
-    return nil
+	sqlDB, err := db.DB()
+	if err != nil {
+		return err
+	}
+	// SetMaxIdleConns 设置空闲连接池中连接的最大数量
+	sqlDB.SetMaxIdleConns(MaxIdleConns)
+	if conf.MaxIdleConns > 0 {
+		sqlDB.SetMaxIdleConns(conf.MaxIdleConns)
+	}
+	// SetMaxOpenConns 设置打开数据库连接的最大数量。
+	sqlDB.SetMaxOpenConns(MaxOpenConns)
+	if conf.MaxOpenConns > 0 {
+		sqlDB.SetMaxOpenConns(conf.MaxOpenConns)
+	}
+	// SetConnMaxLifetime 设置了连接可复用的最大时间。
+	sqlDB.SetConnMaxLifetime(ConnMaxLifetime)
+	if conf.ConnMaxLifetime > 0 {
+		sqlDB.SetConnMaxLifetime(conf.ConnMaxLifetime)
+	}
+	return nil
 }
 
 func GetMysqlClientDB(name string) *gorm.DB {
-    client, ok := mysqlClients[name]
-    if !ok {
-        return nil
-    }
-    return client.db
+	client, ok := mysqlClients[name]
+	if !ok {
+		return nil
+	}
+	return client.db
 }
--- storage/mysql/gorm_test.go
+++ storage/mysql/gorm_test.go
@@ -1,13 +1,13 @@
 package pgorm
 
 import (
-    "context"
-    "fmt"
-    "github.com/leeprince/goinfra/plog"
-    "github.com/leeprince/goinfra/storage/mysql/gorm_test_model"
-    "gorm.io/gorm/logger"
-    "testing"
-    "time"
+	"context"
+	"fmt"
+	"github.com/leeprince/goinfra/plog"
+	"github.com/leeprince/goinfra/storage/mysql/gorm_test_model"
+	"gorm.io/gorm/logger"
+	"testing"
+	"time"
 )
 
 /**
@@ -17,68 +17,68 @@ import (
  */
 
 func initLooger() {
-    // err := plog.SetOutputFile("./logs", "gorm.log", false)
-    err := plog.SetOutputFile("./gorm_test_log", "gorm.log", true)
-    if err != nil {
-        panic(fmt.Sprintf("plog.SetOutputFile err:%v", err))
-    }
-    // plog.SetReportCaller(true)
+	// err := plog.SetOutputFile("./logs", "gorm.log", false)
+	err := plog.SetOutputFile("./gorm_test_log", "gorm.log", true)
+	if err != nil {
+		panic(fmt.Sprintf("plog.SetOutputFile err:%v", err))
+	}
+	// plog.SetReportCaller(true)
 }
 
 func TestInitMysqlClient(t *testing.T) {
-    initLooger()
-    // var logWriterStdout = log.New(os.Stdout, "\r\n", log.LstdFlags) // io writer（日志输出的目标，前缀和日志包含的内容——译者注）
-    var DBLogger = logger.New(
-        // logWriterStdout, // 标准输出
-        plog.GetLogger(), // 指定日志文件输出
-        logger.Config{
-            SlowThreshold:             time.Second, // 慢 SQL 阈值
-            LogLevel:                  logger.Warn, // 日志级别
-            IgnoreRecordNotFoundError: false,       // 忽略ErrRecordNotFound（记录未找到）错误
-            Colorful:                  true,        // 彩色打印
-        },
-    )
-    
-    tmpMysql := "tmp"
-    
-    type args struct {
-        confs MysqlConfs
-    }
-    tests := []struct {
-        name    string
-        args    args
-        wantErr bool
-    }{
-        {
-            args: args{
-                confs: MysqlConfs{
-                    tmpMysql: MysqlConf{
-                        Dsn:     "root:leeprince@tcp(127.0.0.1:3306)/tmp?charset=utf8&parseTime=true&loc=Local&interpolateParams=True",
-                        IsDebug: true,
-                        Logger:  DBLogger,
-                    },
-                }},
-        },
-    }
-    for _, tt := range tests {
-        t.Run(tt.name, func(t *testing.T) {
-            if err := InitMysqlClient(tt.args.confs); (err != nil) != tt.wantErr {
-                t.Errorf("InitMysqlClient() error = %v, wantErr %v", err, tt.wantErr)
-            }
-            
-            tmpDB := GetMysqlClientDB(tmpMysql)
-            if tmpDB == nil {
-                fmt.Println(" GetMysqlClientDB(tmpMysql)")
-                return
-            }
-            
-            userDao := gorm_test_model.NewUsersDao(context.Background(), tmpDB)
-            user, err := userDao.GetByOption(userDao.WithID(1))
-            fmt.Println("GetByOption:", user, err)
-            findName := "name01"
-            users, err := userDao.GetByOptions(userDao.WithName(&findName))
-            fmt.Println("GetByOptions:", users, err)
-            
-        })
-    }
+	initLooger()
+	// var logWriterStdout = log.New(os.Stdout, "\r\n", log.LstdFlags) // io writer（日志输出的目标，前缀和日志包含的内容——译者注）
+	var DBLogger = logger.New(
+		// logWriterStdout, // 标准输出
+		plog.GetLogger(), // 指定日志文件输出
+		logger.Config{
+			SlowThreshold:             time.Second, // 慢 SQL 阈值
+			LogLevel:                  logger.Warn, // 日志级别
+			IgnoreRecordNotFoundError: false,       // 忽略ErrRecordNotFound（记录未找到）错误
+			Colorful:                  true,        // 彩色打印
+		},
+	)
+
+	tmpMysql := "tmp"
+
+	type args struct {
+		confs MysqlConfs
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			args: args{
+				confs: MysqlConfs{
+					tmpMysql: MysqlConf{
+						Dsn:     "root:leeprince@tcp(127.0.0.1:3306)/tmp?charset=utf8&parseTime=true&loc=Local&interpolateParams=True",
+						IsDebug: true,
+						Logger:  DBLogger,
+					},
+				}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := InitMysqlClientMap(tt.args.confs); (err != nil) != tt.wantErr {
+				t.Errorf("InitMysqlClientMap() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			tmpDB := GetMysqlClientDB(tmpMysql)
+			if tmpDB == nil {
+				fmt.Println(" GetMysqlClientDB(tmpMysql)")
+				return
+			}
+
+			userDao := gorm_test_model.NewUsersDao(context.Background(), tmpDB)
+			user, err := userDao.GetByOption(userDao.WithID(1))
+			fmt.Println("GetByOption:", user, err)
+			findName := "name01"
+			users, err := userDao.GetByOptions(userDao.WithName(&findName))
+			fmt.Println("GetByOptions:", users, err)
+
+		})
+	}
 }
