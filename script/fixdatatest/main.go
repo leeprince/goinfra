ee73366fbaa54cb7ca7d1bb542e798ee59bbbcef
prince.lee <prince.lee@fowo.com>
2023-08-14 15:44:12 +0800

    feature: 脚本 flags | 示例

--- script/fixdatatest/main.go
+++ script/fixdatatest/main.go
@@ -19,7 +19,18 @@ go run main.go setcorpid --help
 注意：fixdata是根命令不用写，也不能写
 
 例子：
-go run main.go setcorpid --user_id 20201711 --org_id 0 --order_sn 6669878947741438535 --corp_id=""
+# 测试
+go run main.go setcorpid --user_id 20201711 --org_id 0 --order_sn="6669878947741438535" --corp_id=""
+go run main.go setcorpid --user_id 20201711 --org_id 0 --order_sn="" --corp_id=""
+
+# 生产
+go run main.go setcorpid --user_id 445168 --org_id 488870 --order_sn="7086993366806041064" --corp_id="1609813763522568298"
+go run main.go setcorpid --user_id 17004576 --org_id 488870 --order_sn="" --corp_id="1609813763522568298"
+go run main.go setcorpid --user_id 15896274 --org_id 488870 --order_sn="" --corp_id="1609813763522568298"
+go run main.go setcorpid --user_id 17004576,15896274 --org_id 488870 --order_sn="" --corp_id="1609813763522568298"
+go run main.go setcorpid --user_id 17004576,16428581 --org_id 488870 --order_sn="" --corp_id="1609813763522568298"
+go run main.go setcorpid --user_id 16007312,23935025,16702494,15896309,24621220 --org_id 488870 --order_sn="" --corp_id="1609813763522568298"
+
 */
 func main() {
 	if err := New().Execute(); err != nil {
--- script/fixdatatest/setcorpid/setcorpid.go
+++ script/fixdatatest/setcorpid/setcorpid.go
@@ -3,6 +3,7 @@ package setcorpid
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"strings"
 )
 
 /**
@@ -14,19 +15,19 @@ import (
 type option struct {
 	Env string
 
-	UserId  string
-	OrgId   string
-	OrderSn string
-	CropId  string
+	UserIdList []string
+	OrgId      string
+	OrderSn    string
+	CropId     string
 }
 
 func New() *cobra.Command {
 	rootCmd := &cobra.Command{
 		Use:   "setcorpid",
-		Short: "根据用户ID查找(指定org_id&corp_id为空)的记录，并设置指定corp_id",
+		Short: "根据用户ID查找(指定org_id&order_sn)&corp_id=''的记录，并设置指定corp_id",
 		RunE:  findUserDcacheDataAndSetCorpId,
 	}
-	rootCmd.PersistentFlags().String("user_id", "", "c端用户ID")
+	rootCmd.PersistentFlags().String("user_id", "", "c端用户ID:多个用户时使用英文逗号,分开")
 	rootCmd.PersistentFlags().String("org_id", "", "c端用户ID下指定的org_id")
 	rootCmd.PersistentFlags().String("order_sn", "", "c端用户ID下指定的order_sn")
 	rootCmd.PersistentFlags().String("corp_id", "", "需要设置的corp_id")
@@ -39,7 +40,7 @@ func initFlags(c *cobra.Command) (opt *option, err error) {
 		fmt.Println("env err:", err)
 		return
 	}
-	userId, err := c.PersistentFlags().GetString("user_id")
+	userIdListString, err := c.PersistentFlags().GetString("user_id")
 	if err != nil {
 		fmt.Println("UserId err:", err)
 		return
@@ -61,11 +62,11 @@ func initFlags(c *cobra.Command) (opt *option, err error) {
 	}
 
 	opt = &option{
-		Env:     env,
-		UserId:  userId,
-		OrgId:   orgId,
-		OrderSn: orderSn,
-		CropId:  corpId,
+		Env:        env,
+		UserIdList: strings.Split(userIdListString, ","),
+		OrgId:      orgId,
+		OrderSn:    orderSn,
+		CropId:     corpId,
 	}
 
 	fmt.Printf("opt:%+v \n", opt)
@@ -80,8 +81,8 @@ func findUserDcacheDataAndSetCorpId(c *cobra.Command, _ []string) (err error) {
 		return
 	}
 
-	if opt.UserId == "" {
-		fmt.Println("opt.UserId empty")
+	if len(opt.UserIdList) <= 0 {
+		fmt.Println("opt.UserIdList empty")
 		return
 	}
 	if opt.OrgId == "" {
