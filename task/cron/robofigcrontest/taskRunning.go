af27bfd8aa32b67cea66b14580ccdb25aeee1ccb
prince.lee <prince.lee@fowo.com>
2023-08-21 11:04:21 +0800

    feature: cron

--- task/cron/robofigcrontest/taskRunning.go
+++ task/cron/robofigcrontest/taskRunning.go
@@ -18,18 +18,18 @@ func taskRunning() {
 	// 默认上次任务没运行完，下次任务依然会运行（任务运行在goroutine里相互不干扰）
 	taskRunningDefault()
 
-	//  支持上次任务未执行完，下次任务不启动
+	// 支持上次任务未执行完，下次任务不启动
 	taskRunningSkipIfStillRunning()
 }
 
-//默认上次任务没运行完，下次任务依然会运行（任务运行在goroutine里相互不干扰）
+// 默认上次任务没运行完，下次任务依然会运行（任务运行在goroutine里相互不干扰）
 func taskRunningDefault() {
 	c := NewWithSeconds()
 
 	// 多个定时任务相当于多个协程，使用共享变量一定要注意并发更新的问题。正常处理方法：1.使用sync包加锁；2.使用通道通信自定义锁机制
 	taskId := 0
 
-	//添加一个任务
+	// 添加一个任务
 	// 每3秒执行一次
 	_, err := c.AddFunc("*/1 * * * * ?", func() {
 		log.Println(taskId, "-time.Sleep...")
