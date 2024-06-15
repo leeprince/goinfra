dbfd88a959e8acd977296bc98e06321001f04a32
leeprince <leeprince@foxmail.com>
2024-06-16 01:53:10 +0800

    feature: jwt

--- security/auth/jwtutil/jwttest/create_jwt.go
+++ security/auth/jwtutil/jwttest/create_jwt.go
@@ -30,11 +30,11 @@ func CreateJwtNew() {
 		s   string
 		err error
 	)
-
+	
 	key = []byte(SignedStringKey)
 	t = jwt.New(jwt.SigningMethodHS256)
 	fmt.Printf("t: %+v\n", t)
-
+	
 	s, err = t.SignedString(key)
 	if err != nil {
 		log.Fatal(err)
@@ -47,7 +47,7 @@ func CreateJwtByNewWithClaims() (tokenString string, err error) {
 		key []byte
 		t   *jwt.Token
 	)
-
+	
 	key = []byte(SignedStringKey)
 	t = jwt.NewWithClaims(
 		jwt.SigningMethodHS256,
@@ -62,14 +62,14 @@ func CreateJwtByNewWithClaims() (tokenString string, err error) {
 		},
 	)
 	fmt.Printf("t: %+v\n", t)
-
+	
 	tokenString, err = t.SignedString(key)
 	if err != nil {
 		fmt.Println("SignedString err:", err)
 		return
 	}
 	fmt.Printf("tokenString: %+v\n", tokenString)
-
+	
 	return
 }
 
@@ -77,9 +77,9 @@ func ParseWithClaims(tokenString string) (b bool, err error) {
 	var (
 		key []byte
 	)
-
+	
 	key = []byte(SignedStringKey)
-
+	
 	token, err := jwt.ParseWithClaims(tokenString, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
 		return key, nil
 	})
@@ -87,14 +87,14 @@ func ParseWithClaims(tokenString string) (b bool, err error) {
 		fmt.Println("jwt.ParseWithClaims err:", err)
 		return
 	}
-
+	
 	if claims, ok := token.Claims.(*jwt.RegisteredClaims); ok && token.Valid {
 		fmt.Printf("token.Claims.(*jwt.RegisteredClaims) claims：%v\n", claims)
 		b = true
 	} else {
 		fmt.Println("token.Claims.(*jwt.RegisteredClaims) err:", err)
 	}
-
+	
 	return
 }
 
@@ -103,12 +103,12 @@ func ParseWithClaims(tokenString string) (b bool, err error) {
 // in the custom type to allow for easy encoding, parsing and validation of registered claims.
 func ExampleNewWithClaims_customClaimsType() {
 	mySigningKey := []byte("AllYourBase")
-
+	
 	type MyCustomClaims struct {
 		Foo string `json:"foo"`
 		jwt.RegisteredClaims
 	}
-
+	
 	// Create claims with multiple fields populated
 	claims := MyCustomClaims{
 		"bar",
@@ -123,9 +123,9 @@ func ExampleNewWithClaims_customClaimsType() {
 			Audience:  []string{"somebody_else"},
 		},
 	}
-
+	
 	fmt.Printf("foo: %v\n", claims.Foo)
-
+	
 	// Create claims while leaving out some of the optional fields
 	claims = MyCustomClaims{
 		"bar",
@@ -135,35 +135,36 @@ func ExampleNewWithClaims_customClaimsType() {
 			Issuer:    "test",
 		},
 	}
-
+	
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	ss, err := token.SignedString(mySigningKey)
-	fmt.Printf("%v %v", ss, err)
-
-	//Output: foo: bar
-	//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIiLCJpc3MiOiJ0ZXN0IiwiZXhwIjoxNTE2MjM5MDIyfQ.xVuY2FZ_MRXMIEgVQ7J-TFtaucVFRXUzHm9LmV41goM <nil>
+	fmt.Println("err:", err)
+	fmt.Println("ss:", ss)
+	
+	// Output: foo: bar
+	// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIiLCJpc3MiOiJ0ZXN0IiwiZXhwIjoxNTE2MjM5MDIyfQ.xVuY2FZ_MRXMIEgVQ7J-TFtaucVFRXUzHm9LmV41goM <nil>
 }
 
 // Example creating a token using a custom claims type.  The RegisteredClaims is embedded
 // in the custom type to allow for easy encoding, parsing and validation of standard claims.
 func ExampleParseWithClaims_customClaimsType() {
 	tokenString := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmb28iOiJiYXIiLCJpc3MiOiJ0ZXN0IiwiYXVkIjoic2luZ2xlIn0.QAWg1vGvnqRuCFTMcPkjZljXHh8U3L_qUjszOtQbeaA"
-
+	
 	type MyCustomClaims struct {
 		Foo string `json:"foo"`
 		jwt.RegisteredClaims
 	}
-
+	
 	token, err := jwt.ParseWithClaims(tokenString, &MyCustomClaims{}, func(token *jwt.Token) (interface{}, error) {
 		return []byte("AllYourBase"), nil
 	})
-
+	
 	if claims, ok := token.Claims.(*MyCustomClaims); ok && token.Valid {
 		fmt.Printf("%v %v\n", claims.Foo, claims.RegisteredClaims.Issuer)
 	} else {
 		fmt.Println(err)
 	}
-
+	
 	// Output: bar test
 }
 
